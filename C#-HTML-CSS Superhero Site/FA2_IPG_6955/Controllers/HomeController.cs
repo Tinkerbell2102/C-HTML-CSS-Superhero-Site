094547f883ba1b8762e5ccfe742823060976277e
Laikin Barnard <66776381+Tinkerbell2102@users.noreply.github.com>
2022-01-11 20:28:29 +0200

    C-HTML-CSS-Superhero-Site

---
+++ C#-HTML-CSS Superhero Site/FA2_IPG_6955/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FA2_IPG_6955.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Your location page.";
+
+            return View();
+        }
+
+
+        public ActionResult Favorites()
+        {
+            ViewBag.Message = "Your Favorites page.";
+
+            return View();
+        }
+
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Your Latest news page.";
+
+            return View();
+        }
+    }
+}
---
+++ C#-HTML-CSS Superhero Site/FA2_IPG_6955/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FA2_IPG_6955.MvcApplication" Language="C#" %>
